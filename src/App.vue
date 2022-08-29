f97eca5f35f1336c55228c7e079f32e53aa5d403
Syafades <destianaramadianti@gmail.com>
2022-08-29 15:46:44 +0700

    final cahnge

--- src/App.vue
+++ src/App.vue
@@ -129,9 +129,11 @@ export default {
         <div class="carousel-item active">
           <img :src="titleImage1" class="d-block w-100" alt="iklan1">
           <div class="carousel-caption d-none d-md-block">
-            <div class="col-lg-6">
-              <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                <a href="#"><button type="button" class="btn btn-primary btn-lg px-4">READ MORE</button></a>
+            <div class="read">
+              <div class="col-lg-6">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                  <a href="#"><button type="button" class="btn btn-primary btn-lg px-4">READ MORE</button></a>
+                </div>
               </div>
             </div>
           </div>
@@ -139,9 +141,11 @@ export default {
         <div class="carousel-item">
           <img :src="titleImage2" class="d-block w-100" alt="...">
           <div class="carousel-caption d-none d-md-block">
-            <div class="col-lg-6">
-              <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                <a href="#"><button type="button" class="btn btn-primary btn-lg px-4">READ MORE</button></a>
+            <div class="read">
+              <div class="col-lg-6">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                  <a href="#"><button type="button" class="btn btn-primary btn-lg px-4">READ MORE</button></a>
+                </div>
               </div>
             </div>
           </div>
@@ -149,9 +153,11 @@ export default {
         <div class="carousel-item">
           <img :src="titleImage3" class="d-block w-100" alt="...">
           <div class="carousel-caption d-none d-md-block">
-            <div class="col-lg-6">
-              <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                <a href="#"><button type="button" class="btn btn-primary btn-lg px-4">READ MORE</button></a>
+            <div class="read">
+              <div class="col-lg-6">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                  <a href="#"><button type="button" class="btn btn-primary btn-lg px-4">READ MORE</button></a>
+                </div>
               </div>
             </div>
           </div>
@@ -191,7 +197,7 @@ export default {
               </div>
               <div class="card-body">
                 <h5 class="card-title">{{ data.nama1 }}</h5>
-                <p class="card-text">{{ data.harga1 }}<br>
+                <p class="card-text" style="font-weight: bolder;  font-family: 'Cinzel', serif;">{{ data.harga1 }}<br>
                   <img :src="data.rate1" class="card-img-top" style="width: 80px; height: 15px;" alt="...">
                 </p>
                 <a href="cart.html"><button @click="changeTitle(data)" class="btn btn-primary">
@@ -208,7 +214,7 @@ export default {
   <div class="woman">
     <div class="title">
       <br>
-      <h2><b>WOMAN</b></h2>
+      <h2>WOMAN</h2>
     </div>
     <div class="see">
       <a href="#">
@@ -225,7 +231,7 @@ export default {
               </div>
               <div class="card-body">
                 <h5 class="card-title">{{ data.nama2 }}</h5>
-                <p class="card-text">{{ data.harga2 }}<br>
+                <p class="card-text" style="font-weight: bolder ;  font-family: 'Cinzel', serif;">{{ data.harga2 }}<br>
                   <img :src="data.rate2" class="card-img-top" style="width: 80px; height: 15px;" alt="...">
                 </p>
                 <a href="cart.html"><button @click="changeTitle(data)" class="btn btn-primary">
@@ -242,7 +248,7 @@ export default {
   <div class="man">
     <div class="title">
       <br>
-      <h2><b>MAN</b></h2>
+      <h2>MAN</h2>
     </div>
     <div class="see">
       <a href="#">
@@ -259,7 +265,7 @@ export default {
               </div>
               <div class="card-body">
                 <h5 class="card-title">{{ data.nama3 }}</h5>
-                <p class="card-text">{{ data.harga3 }}<br>
+                <p class="card-text" style="font-weight: bolder ; font-family: 'Cinzel', serif;">{{ data.harga3 }}<br>
                   <img :src="data.rate3" class="card-img-top" style="width: 80px; height: 15px;" alt="...">
                 </p>
                 <a href="cart.html"><button @click="changeTitle(data)" class="btn btn-primary">
@@ -276,7 +282,7 @@ export default {
   <div class="review">
     <div class="title mt-2">
       <br>
-      <h2><b>Review</b></h2>
+      <h1><b>Review</b></h1>
     </div>
     <div class="see">
       <a href="#">All Reviews
@@ -291,7 +297,7 @@ export default {
                 <img :src="data.image" class="img-top " style="width:287px; height: 360px; " alt="...">
               </div>
               <div class="card-body">
-                <h5 class="card-title">{{ data.nama }}</h5>
+                <h5 class="card-title" style="font-weight: bolder ;  font-family: 'Cinzel', serif;">{{ data.nama }}</h5>
                 <p class="card-text"> <img :src="data.rate" class="card-img-top" style="width: 80px; height: 15px;"
                     alt=" ..."><br>
                   {{ data.review }}
@@ -310,77 +316,91 @@ export default {
     <div class="container mt-2">
       <footer class="py-3">
         <div class="row">
-          <div class="col-5 col-md-4 mb-3 ">
-            <h5>CONTACT US</h5>
+          <div class="col-6 col-md-4 mb-3 ">
+            <h5 style="font-weight: bolder ;">CONTACT US</h5>
             <ul class="nav flex-column">
               <li class="nav-item mb-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                   fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
                   <path
                     d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
-                </svg><a href="mailto:dperfum@gmail.com" class="nav-link p-0  "
-                  style="color: white; ">dperfum@gmail.com</a></li>
+                </svg><a href="mailto:dperfum@gmail.com" class="p-2  "
+                  style="color: white; font-family:'Fauna One',serif; ">dperfum@gmail.com</a></li>
               <li class="nav-item mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                   class="bi bi-telephone" viewBox="0 0 16 16">
                   <path
                     d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
-                </svg><a href="#" class="nav-link p-0 " style="color: white;">+62 8116 2347</a>
+                </svg><a class=" p-2 " style="color: white; font-family: 'Fauna One', serif; ">+62 8116 2347</a>
               </li>
               <li class="nav-item mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock"
                   viewBox="0 0 16 16">
                   <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
                   <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
-                </svg><a href="#" class="nav-link p-0 " style="color: white;">Monday - Saturday: 10:00 am to 5pm
+                </svg><a class="p-2" style="color: white;  font-family: 'Fauna One', serif;">Monday - Saturday: 10:00 am
+                  to 5pm
                   (WIB)</a>
               </li>
             </ul>
           </div>
 
           <div class="col-6 col-md-2 mb-3">
-            <h5>HELP</h5>
+            <h5 style="font-weight: bolder ;">HELP</h5>
             <ul class="nav flex-column">
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 " style="color: white;">Terms of Service</a>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 "
+                  style="color: white;  font-family: 'Fauna One', serif;">Terms of Service</a>
               </li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 " style="color: white;">Privacy Policy</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 " style="color: white;">Shipping Policy</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 " style="color: white;">Return Policy</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 "
+                  style="color: white;  font-family: 'Fauna One', serif;">Privacy Policy</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 "
+                  style="color: white;  font-family: 'Fauna One', serif;">Shipping Policy</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 "
+                  style="color: white;  font-family: 'Fauna One', serif;">Return Policy</a></li>
             </ul>
           </div>
 
           <div class="col-6 col-md-2 mb-3">
-            <h5>SHOP</h5>
+            <h5 style="font-weight: bolder ;">SHOP</h5>
             <ul class="nav flex-column">
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 " style="color: white;">Best Sellers</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 " style="color: white;">New Products</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 " style="color: white;">Woman</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 " style="color: white;">Man</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 " style="color: white;">FAQs</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 "
+                  style="color: white;  font-family: 'Fauna One', serif;">Best Sellers</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 "
+                  style="color: white;  font-family: 'Fauna One', serif;">New Products</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 "
+                  style="color: white;  font-family: 'Fauna One', serif;">Woman</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 "
+                  style="color: white;  font-family: 'Fauna One', serif;">Man</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 "
+                  style="color: white;  font-family: 'Fauna One', serif;">FAQs</a></li>
             </ul>
           </div>
 
-          <div class="col-md-3 offset-md-1 mb-3">
+          <div class="col-md-3 offset-md-1 mb-2">
             <form>
-              <h5>NEWSLETTER</h5>
+              <h5 style="font-weight: bolder ;">NEWSLETTER</h5>
               <div class="d-flex flex-column  w-100 ">
-                <label for="newsletter1" class="visually-hidden">Your Email</label>
+                <label for="newsletter1" class="visually-hidden" style="font-family: 'Fauna One', serif;">Your
+                  Email</label>
                 <input id="newsletter1" type="text" class="form-control" placeholder="Your Email">
-                <button class="btn btn-primary" type="button">Join Us</button><br>
+                <button class="btn btn-primary" type="button" style="font-family: 'Fauna One', serif;">Join
+                  Us</button><br>
                 <div class="col-md-5 md-1 mb-3">
-                  <h5>FOLLOW US</h5>
+                  <h5 style="font-weight: bolder ;">FOLLOW US</h5>
                   <ul class="list-unstyled d-flex">
-                    <li class="ms-3"><a class="link-dark" href="twitter.com"><svg xmlns="http://www.w3.org/2000/svg"
+                    <li class="ms-3"><a class="link-dark" href="www.twitter.com"><svg xmlns="http://www.w3.org/2000/svg"
                           width="16" height="16" fill="white" class="bi bi-twitter" viewBox="0 0 16 16">
                           <path
                             d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
                         </svg></a></li>
-                    <li class="ms-3"><a class="link-dark" href="instagram.com"><svg xmlns="http://www.w3.org/2000/svg"
-                          width="16" height="16" fill="white" class="bi bi-instagram" viewBox="0 0 16 16">
+                    <li class="ms-3"><a class="link-dark" href="www.instagram.com"><svg
+                          xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-instagram"
+                          viewBox="0 0 16 16">
                           <path
                             d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
                         </svg></a></li>
-                    <li class="ms-3"><a class="link-dark" href="facebook.com"><svg xmlns="http://www.w3.org/2000/svg"
-                          width="16" height="16" fill="white" class="bi bi-facebook" viewBox="0 0 16 16">
+                    <li class="ms-3"><a class="link-dark" href="www.facebook.com"><svg
+                          xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-facebook"
+                          viewBox="0 0 16 16">
                           <path
                             d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                         </svg></a></li>
@@ -397,10 +417,15 @@ export default {
         </div>
 
         <div class="d-flex flex-column flex-sm-row justify-content-between  border-top">
-          <p>Copyright © 2022 D’Perfum</p>
+          <p style="font-family: 'Fauna One', serif;">Copyright © 2022 D’Perfum</p>
           <ul class="list-unstyled d-flex">
-            <li class="ms-3">
+            <li class="ms-3" style="font-family: 'Fauna One', serif;">
               <p>JL Patal Senayan No. 25, Kebayoran Lama, 12210</p>
+              <p class="float-end"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36"
+                    fill="white" class="bi bi-arrow-bar-up" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                      d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z" />
+                  </svg></a></p>
             </li>
           </ul>
         </div>
@@ -412,6 +437,7 @@ export default {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Cinzel:wght@500&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Fauna+One&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Literata:opsz@7..72&display=swap');
 
 header {
   line-height: 1.5;
@@ -431,7 +457,9 @@ header {
 }
 
 .title {
-  text-decoration-line: underline;
+  border-bottom: 2px black solid;
+  margin-inline-start: 550px;
+  margin-inline-end: 550px;
   text-align: center;
   font-family: 'Cinzel', serif;
 }
@@ -474,6 +502,7 @@ header {
 .bawah {
   background-color: #5C5C5C;
   color: white;
+  font-family: 'Cinzel', serif;
 }
 
 .slide {
@@ -484,9 +513,14 @@ header {
   font-weight: bold;
 }
 
+.read {
+  font-family: 'Literata', serif;
+}
+
 .box {
   margin-left: 20px;
 }
+
 
 .title-name {
   color: black;
