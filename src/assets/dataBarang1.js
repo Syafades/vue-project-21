873e199d2073799b482207207705faf1f32d480e
Syafades <destianaramadianti@gmail.com>
2023-02-05 20:51:35 +0700

    neww

--- src/assets/dataBarang1.js
+++ src/assets/dataBarang1.js
@@ -3,7 +3,7 @@ const DataBarang1 = [
     nama1: "GIORGIO ARMANI SI",
     harga1: "Rp 2.650.000,00",
     image1:
-      href="barang1/img1.png",
+      "barang1/img1.png",
       rate1:
       "https://s3-alpha-sig.figma.com/img/c92d/224d/ccb19a64addfdab4dfdfe2869188663f?Expires=1662336000&Signature=cVcPrAEHV8KmUc7z-Wir4SrvjnInpcSROKfspSHreu5m-WnPZamtWEQ4pSsudvuaOQlulfceWhmjDRlwpTJun71Q5-um0G5~dABotpjVQ-ST~QJLt5Ut-Nzbb-fQR~VlbguZbxwMPoSuYJoYR9uEG8WrMwCZiHYEz8zB734WdpoTrDW-C5xdX~5d9C6EBnuWgXrsnpsvlpaWtfu5DXSpPssKej3-2mkgMjkqI1Mhmz9Yy~G4uMRInOY5smevfhmxpLQs3zxB898CP73fzuX496Uu7FDNf0ZYzkEIctAlTPnantwfXxdPUESpacTiCtx7dZHK7pHkWOTLpaI-ZuxQzw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
   },
